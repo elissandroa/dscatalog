32635d37219b9594ce565bf67389e3d13a8a2e76
elissandroa <elissandroa@hotmail.com>
2023-09-29 20:18:12 -0300

    Product findById

--- backend/src/main/java/edu/elissandro/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/edu/elissandro/dscatalog/resources/ProductResource.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,12 @@ public class ProductResource {
 	@GetMapping
 	public ResponseEntity<Page<ProductDTO>> findAllPaged(Pageable pageable){
 		Page<ProductDTO> dto = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id){
+		ProductDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
 
--- backend/src/main/java/edu/elissandro/dscatalog/services/ProductService.java
+++ backend/src/main/java/edu/elissandro/dscatalog/services/ProductService.java
@@ -1,5 +1,7 @@
 package edu.elissandro.dscatalog.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import edu.elissandro.dscatalog.dto.ProductDTO;
 import edu.elissandro.dscatalog.entities.Product;
 import edu.elissandro.dscatalog.repositories.ProductRepository;
+import edu.elissandro.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -20,5 +23,12 @@ public class ProductService {
 	public Page<ProductDTO> findAllPaged(Pageable pageable) {
 		Page<Product> product = repository.findAll(pageable);
 		return product.map(x -> new ProductDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		Product entity = obj.orElseThrow(() ->  new ResourceNotFoundException("Resource not found"));
+		return new ProductDTO(entity, entity.getCategories());
 	}
 }
