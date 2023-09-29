bcad9ccee218412954fda93641b48a02bc81a290
elissandroa <elissandroa@hotmail.com>
2023-09-29 16:18:35 -0300

    Delete category

--- backend/src/main/java/edu/elissandro/dscatalog/services/CategoryService.java
+++ backend/src/main/java/edu/elissandro/dscatalog/services/CategoryService.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import edu.elissandro.dscatalog.dto.CategoryDTO;
 import edu.elissandro.dscatalog.entities.Category;
 import edu.elissandro.dscatalog.repositories.CategoryRepository;
+import edu.elissandro.dscatalog.services.exceptions.DatabaseException;
 import edu.elissandro.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 
@@ -50,6 +53,19 @@ public class CategoryService {
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found "+ id);
 		} 
+	}
+	
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public void delete (Long id) {
+		if(!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Resource not found");
+		}
+		try {
+			repository.deleteById(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity failure");
+		}
+		
 	}
 	
 
