39e18ac5d4129f210642c3315b97a4b9487fa869
elissandroa <elissandroa@hotmail.com>
2023-09-29 18:18:31 -0300

    Refactor pagination

--- backend/src/main/java/edu/elissandro/dscatalog/services/CategoryService.java
+++ backend/src/main/java/edu/elissandro/dscatalog/services/CategoryService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,8 +24,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Category>categories = repository.findAll(pageRequest);
+	public Page<CategoryDTO> findAllPaged(Pageable pageable) {
+		Page<Category>categories = repository.findAll(pageable);
 		return categories.map(x -> new CategoryDTO(x));
 		}
 	
